81b4c93fb4c1a6c187c62dd10906742309260db7
infan11 <infanjiounrahman20606@gmail.com>
2024-02-04 16:07:06 +0600

    cart get method post method delted method

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ async function run() {
    const reviewCollection = client.db("bistroBossDB").collection("review")
    const cartsCollection = client.db("bistroBossDB").collection("carts")
 // users related api 
+app.get("/users" , async(req, res) => {
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+})
 app.post("/users" , async (req, res) => {
   const user  = req.body;
   // insert email if user doesnot exists:
@@ -42,6 +46,24 @@ app.post("/users" , async (req, res) => {
   }
   const result = await usersCollection.insertOne(user);
   res.send(result); 
+})
+
+app.delete("/users/:id" , async (req, res)  => {
+  const id =  req.params.id;
+  const query = {_id : new  ObjectId(id)};
+  const result = await usersCollection.deleteOne(query)
+  res.send(result)
+})
+app.patch("/users/admin/:id" , async ( req , res ) => {
+  const id = req.params.id;
+  const filter  = {_id : new ObjectId(id)}
+  const updateDoc = {
+    $set:{
+      role: "admin"
+    }
+  }
+  const result = await usersCollection.updateOne(filter , updateDoc);
+  res.send(result)
 })
 //  menu related 
 app.get("/menu" , async(req, res) => {
